810d049a3474451b0046034bfe1ecc6007069e4a
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-18 18:40:14 +0200

    added exposition entity

---
+++ src/main/java/com/anton/expo/repository/entity/Exposition.java
@@ -0,0 +1,11 @@
+package com.anton.expo.repository.entity;
+
+import java.time.LocalDate;
+
+public class Exposition {
+    private long id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
