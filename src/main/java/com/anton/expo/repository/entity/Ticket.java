fa75031ce4163a6f51cd1abf259fdaa7027a2ef3
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-21 16:37:35 +0200

    added ticket entity

---
+++ src/main/java/com/anton/expo/repository/entity/Ticket.java
@@ -0,0 +1,10 @@
+package com.anton.expo.repository.entity;
+
+import java.time.LocalDate;
+
+public class Ticket {
+    private long id;
+    private LocalDate date;
+    private int quantity;
+    private long expositionId;
+}
