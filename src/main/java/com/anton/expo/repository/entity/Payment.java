ecbcd3c9ef6cbfd76e09d498af2eb9c225bd85bf
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-22 19:28:04 +0200

    added Payment entity

---
+++ src/main/java/com/anton/expo/repository/entity/Payment.java
@@ -0,0 +1,9 @@
+package com.anton.expo.repository.entity;
+
+import java.time.LocalDateTime;
+
+public class Payment {
+    private long id;
+    private double total;
+    private LocalDateTime date;
+}
