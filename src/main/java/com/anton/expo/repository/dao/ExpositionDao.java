c396f924ac54a996bd38cd0ba4c3e58d6f2ad1cc
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-29 20:31:48 +0200

    added dao method to search expositions by name

--- src/main/java/com/anton/expo/repository/dao/ExpositionDao.java
+++ src/main/java/com/anton/expo/repository/dao/ExpositionDao.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ExpositionDao extends Dao<Exposition> {
     List<Exposition> getActiveExpositionsForHall(long id);
+
+    List<Exposition> searchByTitle(String query);
 }
--- src/main/java/com/anton/expo/repository/dao/impl/ExpositionDaoImpl.java
+++ src/main/java/com/anton/expo/repository/dao/impl/ExpositionDaoImpl.java
@@ -18,6 +18,8 @@ public class ExpositionDaoImpl implements ExpositionDao {
             "start_date, end_date FROM exposition WHERE exposition_id = ?";
     private final String GET_EXPOSITIONS_FOR_HALL = "SELECT exposition_id, title, description, price, image_path," +
             "start_date, end_date FROM exposition WHERE hall_id = ? AND end_date > ?";
+    private final String SEARCH_EXPOSITIONS_BY_TITLE = "SELECT exposition_id, title, description, price, image_path," +
+            "start_date, end_date FROM exposition WHERE title LIKE %?% AND end_date > ?";
 
     public ExpositionDaoImpl(Connection connection) {
         this.connection = connection;
@@ -91,6 +93,33 @@ public class ExpositionDaoImpl implements ExpositionDao {
         } catch (SQLException e) {
             LOG.error("Extraction of active expositions for hall failed.", e);
             throw new ExpositionException("Can't get active expositions for hall: " + e.getMessage(), e);
+        }
+        return expositions;
+    }
+
+    @Override
+    public List<Exposition> searchByTitle(String query) {
+        List<Exposition> expositions = new ArrayList<>();
+        try (PreparedStatement statement = this.connection.prepareStatement(SEARCH_EXPOSITIONS_BY_TITLE)) {
+            statement.setString(1, query);
+            statement.setDate(2, Date.valueOf(LocalDate.now()));
+
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Exposition exposition = new Exposition();
+                exposition.setId(rs.getLong("exposition_id"));
+                exposition.setTitle(rs.getString("title"));
+                exposition.setDescription(rs.getString("description"));
+                exposition.setPrice(rs.getDouble("price"));
+                exposition.setImagePath(rs.getString("image_path"));
+                exposition.setStartDate(rs.getDate("start_date").toLocalDate());
+                exposition.setEndDate(rs.getDate("end_date").toLocalDate());
+
+                expositions.add(exposition);
+            }
+        } catch (SQLException e) {
+            LOG.error("Extraction of searched expositions for hall failed.", e);
+            throw new ExpositionException("Can't get searched expositions for hall: " + e.getMessage(), e);
         }
         return expositions;
     }
