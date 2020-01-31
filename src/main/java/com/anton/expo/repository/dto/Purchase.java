25819cdccfebe8090b35518f6e9d993a19a99315
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-31 21:30:02 +0200

    added purchase dto

---
+++ src/main/java/com/anton/expo/repository/dto/Purchase.java
@@ -0,0 +1,18 @@
+package com.anton.expo.repository.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Purchase {
+    private long cardNumber;
+    private LocalDateTime date;
+    private double total;
+    private List<TicketDto> tickets;
+}
